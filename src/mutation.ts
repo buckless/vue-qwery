7daa7935a3b42d87f7b0c00def1b2df3fa37c0e8
Lucas Soulier <lucas@buckless.com>
2020-05-25 16:51:09 +0200

    fix(mutation): fix useMutation return type

--- src/mutation.ts
+++ src/mutation.ts
@@ -1,13 +1,22 @@
-import { ref } from "@vue/composition-api";
+import { ref, Ref } from "@vue/composition-api";
 import axios, { AxiosResponse, AxiosError, Method } from "axios";
 
 export type MutationStatus = "idle" | "loading" | "error" | "done";
+
+type UseMutationResult<TBody, TResponse> = [
+  (body: TBody) => Promise<AxiosResponse<TResponse>>,
+  {
+    status: Ref<MutationStatus>;
+    data: Ref<AxiosResponse<TResponse | undefined>>;
+    error: Ref<AxiosError<TResponse> | undefined>;
+  }
+];
 
 export function useMutation<TBody, TResponse>(
   url: string,
   method: Method = "POST",
   headers: Record<string, string> = {}
-) {
+): UseMutationResult<TBody, TResponse> {
   const status = ref<MutationStatus>("idle");
   const data = ref<AxiosResponse<TResponse | undefined>>(undefined);
   const error = ref<AxiosError<TResponse> | undefined>(undefined);
